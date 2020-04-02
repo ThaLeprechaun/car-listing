f97ecc888d2b3673861e6b09d843bf62f5ec66cb
thaleprechaun <oakinbiyiusman@gmail.com>
2020-04-02 09:34:50 +0100

    feat(filter routes): Add routes to create a new record and to view filtered record

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,72 @@
 import { Router } from 'express';
-import sampleController from '../controllers/sample';
+import {
+  firstFilter,
+  createOwner,
+  secondFilter,
+  thirdFilter,
+  fourthFilter,
+} from '../controllers/carController';
 
 const router = Router();
 
-router.get('/', function(_req, res, _next) {
-  const message = sampleController();
+router.post('/', async function(req, res) {
+  try {
+    const newOwner = await createOwner(req.body);
+    return res.status(200).json({ message: 'success', newOwner });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    return;
+  }
+});
 
-  res.status(200).json({ message });
+router.get('/first-filter', async function(_req, res) {
+  try {
+    const carOwner = await firstFilter();
+    return res.status(200).json({ message: 'success', carOwner });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    return;
+  }
+});
+
+router.get('/second-filter', async function(_req, res) {
+  try {
+    const carOwner = await secondFilter();
+    return res.status(200).json({ message: 'success', carOwner });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    return;
+  }
+});
+
+router.get('/third-filter', async function(_req, res) {
+  try {
+    const carOwner = await thirdFilter();
+    return res.status(200).json({ message: 'success', carOwner });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    return;
+  }
+});
+
+router.get('/fourth-filter', async function(_req, res) {
+  try {
+    const carOwner = await fourthFilter();
+    return res.status(200).json({ message: 'success', carOwner });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    return;
+  }
+});
+
+router.get('/fifth-filter', async function(_req, res) {
+  try {
+    const carOwner = await fourthFilter();
+    return res.status(200).json({ message: 'success', carOwner });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    return;
+  }
 });
 
 export default router;
