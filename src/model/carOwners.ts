8589837fb3b0e3cab06f12d9c0ad2a84ae141eee
thaleprechaun <oakinbiyiusman@gmail.com>
2020-04-02 09:31:54 +0100

    feat(Add model): create data model for the app

---
+++ src/model/carOwners.ts
@@ -0,0 +1,63 @@
+import mongoose from 'mongoose';
+
+export interface CarSchema extends mongoose.Document {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  country: string;
+  car_model: string;
+  car_model_year: number;
+  car_color: string;
+  gender: string;
+  job_title: string;
+  bio: string;
+}
+
+const carSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  car_model: {
+    type: String,
+    required: true,
+  },
+  car_model_year: {
+    type: String,
+    required: true,
+  },
+  car_color: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  job_title: {
+    type: String,
+    required: true,
+  },
+  bio: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model<CarSchema>('carOwners', carSchema);
