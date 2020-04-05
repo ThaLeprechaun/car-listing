2ee02bbfe2e916225bd14a78857976cd817e6cb1
thaleprechaun <oakinbiyiusman@gmail.com>
2020-04-05 22:05:27 +0100

    rename file

--- __tests__/routes.ts
+++
@@ -1,8 +0,0 @@
-import request from 'supertest';
-import app from '../src/app';
-import { connect, disconnect } from '../scripts/mongo-setup';
-require('dotenv').config();
-process.env = JSON.parse(JSON.stringify(process.env));
-
-beforeAll(connect);
-afterAll(disconnect);
